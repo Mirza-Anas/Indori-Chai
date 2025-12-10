9255d506d5c99b27eb593acac8b816c2f2d2f3cf
Mirza-Anas <mirzanas194@gmail.com>
2025-12-10 18:25:42 +0530

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  output: "export",
+  basePath: "/Indori-Chai",
+  assetPrefix: "/Indori-Chai/",
   reactCompiler: true,
 };
 
